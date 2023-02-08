6de2d8a8dc2f55d3c61094a13fcbfd23070b3b5a
LibelluleRose <0729193@cegepoutaouais.qc.ca>
2023-02-08 16:10:26 -0500

    Réglé beaucoup d'erreur mais fonctionne toujours pas.

--- script2.js
+++ script2.js
@@ -18,51 +18,61 @@ class Fruit {
 
 //Fonction qui calcule le montant final
 function CalculerMontantFinal() {
-    let MontantTotalFinal = NbSacPeches.CalculerSousTotal() +
-        NbSacPoires.CalculerSousTotal() +
-        NbSacPommes.CalculerSousTotal();
+    let MontantTotalFinal = SacPeches.CalculerSousTotal() +
+        SacPoires.CalculerSousTotal() +
+        SacPommes.CalculerSousTotal();
 
     return MontantTotalFinal;
 
 }
 
 //Fonction qui met à jour et affiche les différents totaux
-function MiseAJour() {
-    //Ajouter sac
-    SacPeches.qte = SacPeches.qte + 1;
+function MiseAJour(fruit) {
 
-
-    //mise à jour des sous-totaux
-    let totalpeche = NbSacPeches.CalculerSousTotal();
-    let totalpoire = NbSacPoires.CalculerSousTotal();
-    let totalpomme = NbSacPommes.CalculerSousTotal();
-
-    document.getElementById("totalPeches").textContent = totalpeche;
-    document.getElementById("totalPoires").textContent = totalpoire;
-    document.getElementById("totalPommes").textContent = totalpomme;
+    //Ajouter sac pour le fruit rentré en param
+    //calculer le nouveau sous-total
+    //Inscrire le nouveau sous-total
+    if (fruit === "peche"){
+        SacPeches.qte = SacPeches.qte + 1;
+        let totalpeche = SacPeches.CalculerSousTotal();
+        document.getElementById("totalPeches").textContent = totalpeche;
+    }
+    else if (fruit === "Poires"){
+        SacPoires.qte = SacPoires.qte + 1;
+        let totalpoire = SacPoires.CalculerSousTotal();
+        document.getElementById("totalPoires").textContent = totalpoire;
+    }
+    else if (fruit === "Pommes"){
+        SacPommes.qte = SacPommes.qte + 1;
+        let totalpomme = SacPommes.CalculerSousTotal();
+        document.getElementById("totalPommes").textContent = totalpomme;
+    }
 
     //mise à jour du total
-    let total = CalculerMontantFinal()
-    document.querySelector(#total).textContent = MontantTotalFinal;
+    let total = CalculerMontantFinal() //calculer montant
+    document.getElementById("total").textContent = total; //inscrire montant
 }
 
-///////////////////////////////////////////////////////////////////////////////
-//Programme principale
-//////////////////////////////////////////////////////////////////////////////
 
-//Création de trois fruit à partir de la class fruit
-let NbSacPeches = document.getElementById("nbPeches").value;
-let NbSacPoires = document.getElementById("nbPoires").value;
-let NbSacPommes = document.getElementById("nbPommes").value;
+function load(){
 
-let PrixSacPeches = document.getElementById("prixPeches").value;
-let PrixSacPoires = document.getElementById("prixPoires").value;
-let PrixSacPommes = document.getElementById("prixPommes").value;
+    //Création de trois fruit à partir de la class fruit
+    let NbSacPeches = +document.getElementById("nbPeches").value; //le + c'est pour déclarer que c'est une valeur numérique
+    let NbSacPoires = +document.getElementById("nbPoires").value;
+    let NbSacPommes = +document.getElementById("nbPommes").value;
+
+    let PrixSacPeches = +document.getElementById("prixPeches").value;
+    let PrixSacPoires = +document.getElementById("prixPoires").value;
+    let PrixSacPommes = +document.getElementById("prixPommes").value;
+
+    SacPeches = new Fruit("SacPeches", PrixSacPeches, NbSacPeches);
+    SacPoires = new Fruit("SacPoires", PrixSacPoires, NbSacPoires);
+    SacPommes = new Fruit("SacPommes", PrixSacPommes, NbSacPommes);
+
+}
 
 
-const SacPeches = new Fruit(SacPeches, PrixSacPeches, NbSacPeches);
-const SacPoires = new Fruit(SacPoires, PrixSacPoires, NbSacPoires);
-const SacPommes = new Fruit(SacPommes, PrixSacPommes, NbSacPommes);
+
 
 
 

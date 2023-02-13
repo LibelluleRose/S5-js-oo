09812fbd5d3ad2af8cde7b5df8af9a291515000b
LibelluleRose <0729193@cegepoutaouais.qc.ca>
2023-02-12 23:50:12 -0500

    je suis en train d'utiliser le débogueur pas à pas pour voir où est-ce que ça bloque.

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@
 class Fruit {
 
     //Constructeur
-    Fruit(nom = "", prix = 0, qte = 0) {
+    Fruit(nom = "Fruit", prix = 0, qte = 0) {
         this.nom = nom;
         this.prix = prix;
         this.qte = qte;
@@ -11,16 +11,16 @@ class Fruit {
 
     //Méthode qui calcule et affiche le sous-total
     CalculerSousTotal() {
-        let newSousTotal = this.prix * this.qte;
-        return newSousTotal;
+        let newSousTotal = +this.prix * +this.qte;
+        return +newSousTotal;
     }
 }
 
 //Fonction qui calcule le montant final
 function CalculerMontantFinal() {
-    let MontantTotalFinal = SacPeches.CalculerSousTotal() +
-        SacPoires.CalculerSousTotal() +
-        SacPommes.CalculerSousTotal();
+    let MontantTotalFinal = +(SacPeches.CalculerSousTotal()) +
+        +(SacPoires.CalculerSousTotal()) +
+            +(SacPommes.CalculerSousTotal());
 
 
     return MontantTotalFinal;
@@ -35,48 +35,54 @@ function MiseAJour(fruit) {
     //Inscrire le nouveau sous-total
     if (fruit === "Peche"){
         // SacPeches.qte = SacPeches.qte + 1; //lire le inuput au lieu de faire +1
-        let qtepeche = document.getElementById("nbPeches").values;
+        SacPeches.qte = +document.getElementById("nbPeches").value;
+        SacPeches.prix = parseInt(document.getElementById("prixPeches").value);
         totalpeche = SacPeches.CalculerSousTotal();
         document.querySelector("#nbPeches").textContent = totalpeche + " $";
     }
     else if (fruit === "Poires"){
-        SacPoires.qte = SacPoires.qte + 1;
+        SacPoires.qte = +document.getElementById("nbPoires").value;
+        SacPoires.prix = +document.getElementById("prixPoires").value;
         totalpoire = SacPoires.CalculerSousTotal();
         document.querySelector("#nbPoires").textContent = totalpoire + " $";
     }
     else if (fruit === "Pommes"){
-        SacPommes.qte = SacPommes.qte + 1;
+        SacPommes.qte = +document.getElementById("nbPommes").value;
+        SacPommes.prix = +document.getElementById("prixPommes").value;
         totalpomme = SacPommes.CalculerSousTotal();
         document.querySelector("#nbPommes").textContent = totalpomme + " $";
     }
 
     //mise à jour du total
     let total = CalculerMontantFinal() //calculer montant
-    parseInt(document.getElementById("total").textContent) = +total + " $"; //inscrire montant
+    document.getElementById("total").textContent = +total + " $"; //inscrire montant
     //parseInt(document.getElementById("id ici").textcontent)//exemple de pierre-luc
 }
 
 
 function load(){
 
-    //Création de trois fruit à partir de la class fruit
-    const NbSacPeches = parseInt().document.getElementById("nbPeches").value; //le + c'est pour déclarer que c'est une valeur numérique
-    const NbSacPoires = document.getElementById("nbPoires").value;
-    const NbSacPommes = document.getElementById("nbPommes").value;
+    /*//Création de trois fruit à partir de la class fruit
+    const NbSacPeches = 0;//document.getElementById("nbPeches").value; //le + c'est pour déclarer que c'est une valeur numérique
+    const NbSacPoires = 0;//document.getElementById("nbPoires").value;
+    const NbSacPommes = 0;//document.getElementById("nbPommes").value;
 
-    const PrixSacPeches = document.getElementById("prixPeches").value;
-    const PrixSacPoires = document.getElementById("prixPoires").value;
-    const PrixSacPommes = document.getElementById("prixPommes").value;
+    const PrixSacPeches = 0;//document.getElementById("prixPeches").value;
+    const PrixSacPoires = 0;//document.getElementById("prixPoires").value;
+    const PrixSacPommes = 0;//document.getElementById("prixPommes").value;
 
     SacPeches = new Fruit("SacPeches", PrixSacPeches, NbSacPeches);
     SacPoires = new Fruit("SacPoires", PrixSacPoires, NbSacPoires);
     SacPommes = new Fruit("SacPommes", PrixSacPommes, NbSacPommes);
-
+*/
+    SacPeches = new Fruit("SacPeches", 0, 0);
+    SacPoires = new Fruit("SacPoires", 0, 0);
+    SacPommes = new Fruit("SacPommes", 0, 0);
 }
 
 
-sessionStorage.setItem("panier", panier);
-return true
+//sessionStorage.setItem("panier", panier);
+//return true
 
 
 
